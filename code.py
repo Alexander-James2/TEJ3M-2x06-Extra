7f44a9bafc3d7203ab3c159c59166008be8edb9c
Alexander-James2 <144252140+Alexander-James2@users.noreply.github.com>
2023-11-29 11:15:42 -0500

    final code

--- code.py
+++ code.py
@@ -1,23 +1,16 @@
-from machine import Pin, time_pulse_us
+# Alexander James
+# Nov. 2023
+# Controls a distance sensor using a Pi Pico
+
+# This imports required libraries and defines the pins
 import time
+import board
+import adafruit_hcsr04
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.A1, echo_pin=board.A0)
 
-SOUND_SPEED=340 # Vitesse du son dans l'air
-TRIG_PULSE_DURATION_US=10
-
-trig_pin = Pin(15, Pin.OUT) # Broche GP15 de la Pico
-echo_pin = Pin(14, Pin.IN)  # Broche GP14 de la Pico
- 
 while True:
-    # Prepare le signal
-    trig_pin.value(0)
-    time.sleep_us(5)
-    # Créer une impulsion de 10 µs
-    trig_pin.value(1)
-    time.sleep_us(TRIG_PULSE_DURATION_US)
-    trig_pin.value(0)
-
-    ultrason_duration = time_pulse_us(echo_pin, 1, 30000) # Renvoie le temps de propagation de l'onde (en µs)
-    distance_cm = SOUND_SPEED * ultrason_duration / 20000
-
-    print(f"Distance : {distance_cm} cm")
-    time.sleep_ms(500)
+    try:
+        print((sonar.distance,))
+    except RuntimeError:  # Prevents crashing when an error occurs 
+        print("Retrying!")
+    time.sleep(0.1)
